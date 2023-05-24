30b2f885e914de3ce01e2a1feebe93b2290ddce4
{Jun <{planetes215@gmail.com}>
2023-05-24 15:17:10 +0900

    rename code name

--- debug/kldiv_compare_attention.py
+++ debug/kldiv_compare_attention.py
@@ -7,13 +7,13 @@ import argparse
 def calc_kldiv(a_vec, b_vec):
     return np.sum([a * np.log(a/b) for a, b in zip(a_vec, b_vec)])
 
-def calc_v_attention_kldiv(L2_attention,L9_attention):
+def calc_v_attention_kldiv(L2_attention,max_L_attention):
     output = []
-    for v_attention_l2, v_attention_l9 in zip(L2_attention, L9_attention):
+    for v_attention_l2, v_attention_max_l in zip(L2_attention, max_L_attention):
         epsilon_vec = np.array([1e-5 for _ in range(v_attention_l2.shape[0])])
         v_attention_l2 += epsilon_vec
-        v_attention_l9 += epsilon_vec
-        kl_div = calc_kldiv(v_attention_l2, v_attention_l9)
+        v_attention_max_l += epsilon_vec
+        kl_div = calc_kldiv(v_attention_l2, v_attention_max_l)
         output.append(kl_div)
     return output
 
@@ -32,10 +32,10 @@ def visualize_attention_kldiv(kl_divs,save_dir,output_name,outlier):
 
 def load_attention(args):
     DeepGAT_L2_Attention = np.load(args.DeepGAT_L2_att, allow_pickle=True)
-    DeepGAT_L9_Attention = np.load(args.DeepGAT_L9_att, allow_pickle=True)
+    DeepGAT_max_L_Attention = np.load(args.DeepGAT_max_L_att, allow_pickle=True)
     GAT_L2_Attention = np.load(args.GAT_L2_att, allow_pickle=True)
-    GAT_L9_Attention = np.load(args.GAT_L9_att, allow_pickle=True)
-    return DeepGAT_L2_Attention,DeepGAT_L9_Attention,GAT_L2_Attention,GAT_L9_Attention
+    GAT_max_L_Attention = np.load(args.GAT_max_L_att, allow_pickle=True)
+    return DeepGAT_L2_Attention,DeepGAT_max_L_Attention,GAT_L2_Attention,GAT_max_L_Attention
     
     
 
@@ -45,20 +45,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', type=str, default='None')
     parser.add_argument('--DeepGAT_L2_att', type=str, default='None')
-    parser.add_argument('--DeepGAT_L9_att', type=str, default='None')
+    parser.add_argument('--DeepGAT_max_L_att', type=str, default='None')
     parser.add_argument('--GAT_L2_att', type=str, default='None')
-    parser.add_argument('--GAT_L9_att', type=str, default='None')
+    parser.add_argument('--GAT_max_L_att', type=str, default='None')
     parser.add_argument('--output_name', type=str, default='None')
     parser.add_argument('--outlier', type=str, default='')
     args = parser.parse_args()
 
     
-    DeepGAT_L2_Attention,DeepGAT_L9_Attention,GAT_L2_Attention,GAT_L9_Attention = load_attention(args)
+    DeepGAT_L2_Attention,DeepGAT_max_L_Attention,GAT_L2_Attention,GAT_max_L_Attention = load_attention(args)
     
     
     kl_divs = {}
-    kl_divs["DeepGAT (DP)"] = calc_v_attention_kldiv(DeepGAT_L2_Attention,DeepGAT_L9_Attention)
-    kl_divs["GAT (DP)"]     = calc_v_attention_kldiv(GAT_L2_Attention,GAT_L9_Attention)
+    kl_divs["DeepGAT (DP)"] = calc_v_attention_kldiv(DeepGAT_L2_Attention,DeepGAT_max_L_Attention)
+    kl_divs["GAT (DP)"]     = calc_v_attention_kldiv(GAT_L2_Attention,GAT_max_L_Attention)
     print(f"dataset:{args.name}")
     visualize_attention_kldiv(kl_divs,save_dir,args.output_name,args.outlier)
     
